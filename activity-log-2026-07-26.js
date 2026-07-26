aba73382104f974c0c13ee7e119b7f9ed3d1297b
Yilkash <260288456+Yilkash@users.noreply.github.com>
2026-07-26 08:09:04 +0100

    chore: CORE: Prepare integration for mainnet hooks

--- activity-log-2026-07-26.js
+++ activity-log-2026-07-26.js
@@ -13,3 +13,4 @@
 // TODO: Refactor state management - 2026-07-26T07:09:03.997Z
 // TEST: Add unit tests for this module - 2026-07-26T07:09:04.011Z
 // TODO: Refactor state management - 2026-07-26T07:09:04.023Z
+// CORE: Prepare integration for mainnet hooks - 2026-07-26T07:09:04.036Z
